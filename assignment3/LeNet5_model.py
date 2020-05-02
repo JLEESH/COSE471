470f2e3f992e9518fcf0cbc10dbef22fb0a863da
JLEESHGM <jimmyleeshgm@gmail.com>
2020-05-02 23:32:03 +0900

    fixed LeNet

--- assignment3/LeNet5_model.py
+++ assignment3/LeNet5_model.py
@@ -12,13 +12,15 @@ class LeNet5_model(nn.Module):
         
         # define layers as shown in the PDF file
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding=0)
-        self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0)
-        self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.fcn1 = nn.Linear(in_features=16 * 5 * 5, out_features=120)
         self.relu1 = nn.ReLU()
-        self.fcn2 = nn.Linear(in_features=120, out_features=84)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0)
         self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fcn1 = nn.Linear(in_features=16 * 5 * 5, out_features=120)
+        self.frelu1 = nn.ReLU()
+        self.fcn2 = nn.Linear(in_features=120, out_features=84)
+        self.frelu2 = nn.ReLU()
         
         # gaussian connections replaced with FCN + CrossEntropyLoss
         self.fcn3 = nn.Linear(in_features=84, out_features=10)
@@ -34,13 +36,15 @@ class LeNet5_model(nn.Module):
         
         # feed forward
         x = self.conv1(x)
+        x = self.relu1(x)
         x = self.pool1(x)
         x = self.conv2(x)
+        x = self.relu2(x)
         x = self.pool2(x)
         x = self.fcn1(x)
-        x = self.relu1(x)
+        x = self.frelu1(x)
         x = self.fcn2(x)
-        x = self.relu2(x)
+        x = self.frelu2(x)
         x = self.fcn3(x)
 
         ###############################################################################################################
